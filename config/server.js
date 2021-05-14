6fd526c38e92a41a98f81c0d8b6b3c7c34487365
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-14 22:00:48 +0100

    Intitial commit - Strapi set up and linked to Cloudinary

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '468c745615407c30735f8e212b96f307'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9a64a480-fcd1-4cec-8f36-139d6f534d81'
+};
