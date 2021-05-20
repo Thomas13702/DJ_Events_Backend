ed2cbcf4b359450d38966d7181055ffd453bfd1d
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-20 21:00:09 +0100

    Addec custom Events Endpoint in strapi backend

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  //Get logged in users
+  async me(ctx) {
+    const user = ctx.state.user; //gets the user
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorisation header was found" }] },
+      ]);
+    }
+
+    const data = await strapi.services.events.find({ user: user.id }); //get specific user's events
+
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    return sanitizeEntity(data, { model: strapi.models.events });
+  },
+};
